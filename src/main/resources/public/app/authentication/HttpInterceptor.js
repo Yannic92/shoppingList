f262ba4993ec6d098a6f794bb92684f085b10740
Yannic Klem <yann.klem@gmail.com>
2015-11-10 00:24:05 +0100

    detect session timeout in ui and redirect to login

--- src/main/resources/public/app/authentication/HttpInterceptor.js
+++ src/main/resources/public/app/authentication/HttpInterceptor.js
@@ -1,11 +1,24 @@
-shoppingList.service('MyHttpInterceptor',['$location', '$q', '$injector', '$rootScope',
-    function ($location ,$q, $injector, $rootScope) {
+shoppingList.service('MyHttpInterceptor',['$location', '$q', '$injector', '$rootScope','$window',
+    function ($location ,$q, $injector, $rootScope,$window) {
         return {
             responseError: function(rejection) {
                 if(rejection.status == 401 || rejection.status == 403) {
                     
                     if($rootScope.authenticated) {
-                        console.log("Session timed out")
+                        var $mdDialog = $injector.get('$mdDialog');
+                        $mdDialog.show(
+                            $mdDialog.alert()
+                                .parent(angular.element(document.querySelector('#popupContainer')))
+                                .clickOutsideToClose(true)
+                                .title('Sitzung abgelaufen')
+                                .content('Ihre Sitzung ist abgelaufen. Bitte melden Sie sich erneut an')
+                                .ariaLabel('Sitzung abgelaufen')
+                                .ok('OK')
+                        ).then(function(){
+                                $rootScope.authenticated = false;
+                                $location.path("/login");
+                                $window.location.reload()
+                            });
                     }else{
                         $location.path('/login');
                     }
