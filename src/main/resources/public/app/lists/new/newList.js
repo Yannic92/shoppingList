4e4446bd8d9d5aa7038cdf3698aa8c079c18e1ef
Yannic Klem <klem@synyx.de>
2016-01-04 15:59:18 +0100

    solves #2 - hide remove button for current user

--- src/main/resources/public/app/lists/new/newList.js
+++ src/main/resources/public/app/lists/new/newList.js
@@ -1,6 +1,6 @@
 shoppingList.controller('newList', ['$scope','$rootScope','listService','$mdToast','$location','userService','$mdDialog',
     function ($scope, $rootScope, listService,$mdToast,$location,userService,$mdDialog) {
-        
+
         $rootScope.title = "Neue Einkaufsliste";
         $scope.loading = false;
         $scope.userSearchText = "";
@@ -9,9 +9,9 @@ shoppingList.controller('newList', ['$scope','$rootScope','listService','$mdToas
             name: "",
             owners: [$rootScope.user]
         };
-        
+
         $scope.users = userService.get();
-        
+
         $scope.hasProperty = function (user, value,list) {
             var filter = $scope.userSearchText;
             var concatenatedFirstAndLastName = null;
@@ -20,33 +20,33 @@ shoppingList.controller('newList', ['$scope','$rootScope','listService','$mdToas
             }
             return user.username.toUpperCase().indexOf(filter.toUpperCase()) >= 0 ||
             (user.firstName && user.firstName.toUpperCase().indexOf(filter.toUpperCase()) >= 0) ||
-            (user.lastName && user.lastName.toUpperCase().indexOf(filter.toUpperCase()) >= 0)|| 
+            (user.lastName && user.lastName.toUpperCase().indexOf(filter.toUpperCase()) >= 0)||
             (concatenatedFirstAndLastName && concatenatedFirstAndLastName.toUpperCase().indexOf(filter.toUpperCase()) >= 0);
         };
-        
+
         $scope.notContained = function (user){
             for(var i = 0; i < $scope.list.owners.length; i++){
                 if(user.username == $scope.list.owners[i].username){
                     return false;
                 }
             }
-            
+
             return true;
         };
-        
+
         var fetchUsersIfNecessary = function(){
             if(!$scope.users.loaded){
                 userService.fetch();
             }
         };
-        
+
         $scope.firstNameOrLastNameIsDefined = function(user){
-            
+
             return user.firstName || user.lastName;
         };
-        
+
         fetchUsersIfNecessary();
-        
+
         $scope.createList = function () {
             $scope.loading = true;
             listService.create($scope.list)
@@ -63,7 +63,7 @@ shoppingList.controller('newList', ['$scope','$rootScope','listService','$mdToas
                     $scope.loading = false;
                 });
         };
-        
+
         $scope.addUserToOwners = function (selectedUser) {
             if(selectedUser){
                 $scope.list.owners.push(selectedUser);
@@ -71,22 +71,15 @@ shoppingList.controller('newList', ['$scope','$rootScope','listService','$mdToas
 
             $scope.userSearchText = "";
         };
-        
+
         $scope.removeUserFromOwners = function (index, user){
-            if(user.username == $rootScope.user.username){
-                $mdDialog.show(
-                    $mdDialog.alert()
-                        .parent(angular.element(document.querySelector('#popupContainer')))
-                        .clickOutsideToClose(true)
-                        .title('Ungültig')
-                        .content('Du musst zu den berechtigten Personen der zu erstellenden Einkaufsliste gehören.')
-                        .ok('OK')
-                )
-            }else {
-                $scope.list.owners.splice(index, 1);
-            }
+            $scope.list.owners.splice(index, 1);
         };
-        
+
+        $scope.isLoggedInUser = function (user) {
+            return user && user.username === $rootScope.user.username;
+        };
+
         $scope.disableCreateButton = function(createShoppingListForm){
             return !createShoppingListForm || !createShoppingListForm.$valid || createShoppingListForm.$pristine || $scope.list.owners.length == 0;
         };
