b2de100e987c1299c71c23a6a8f5d08aa7985dc3
Yannic Klem <klem@synyx.de>
2016-01-06 12:11:28 +0100

    ng-view should have full height available

--- src/main/resources/public/app/lists/lists.js
+++ src/main/resources/public/app/lists/lists.js
@@ -12,23 +12,19 @@ shoppingList.controller('lists', ['$scope', '$rootScope','listService',
         'use strict';
 
         $rootScope.title = "Einkaufslisten";
-        
+        $scope.loading = true;
+
         var lists = listService.get();
-        
+
         lists.promise.then(function(){
-            
             if(!$scope.listsAreEmpty()){
                 $scope.$parent.goto("/lists/" + lists[0].entityId);
             }
+            $scope.loading = false;
         });
-        
+
         $scope.listsAreEmpty = function(){
             return !lists || !lists.length || lists.length == 0;
         };
-        
-        $scope.goToFirstList = function (){
-            
-            
-        }
     }
 ]);
