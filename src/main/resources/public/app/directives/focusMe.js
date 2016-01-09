457e0149021d620a51e84fbff1a88640e156971c
Yannic Klem <yann.klem@gmail.com>
2016-01-09 19:13:54 +0100

    added focus-me directive

---
+++ src/main/resources/public/app/directives/focusMe.js
@@ -0,0 +1,15 @@
+shoppingList.directive('focusMe', function($timeout) {
+    return {
+        link: function(scope, element, attrs) {
+            scope.$watch(attrs.focusMe, function(value) {
+                if(value === true) {
+                    console.log('value=',value);
+                    $timeout(function() {
+                        element[0].focus();
+                        scope[attrs.focusMe] = false;
+                    });
+                }
+            });
+        }
+    };
+});
