0ee935e80c97b7f14e96bbe161786f9f17f4700d
Yannic Klem <klem@synyx.de>
2016-01-18 07:51:38 +0100

    fixed ordering of elemnts and show loading cycle on deleting items

--- src/main/resources/public/app/item/item.js
+++ src/main/resources/public/app/item/item.js
@@ -1,13 +1,18 @@
 shoppingList.controller('itemController', ['$scope','listService','itemService',
     function ($scope,listService,itemService) {
         'use strict';
-        
+
         $scope.deleteItem = function(item, event) {
 
+            item.deleting = true;
             itemService.delete(item)
                 .then(function(){
                     var index = $scope.list.items.indexOf(item);
                     $scope.$parent.list.items.splice(index, 1);
+                    return item;
+                })
+                .finally(function () {
+                    item.deleting = false;
                 });
 
         };
