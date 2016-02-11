7cb556d7111d919ab92fcfbdd70e449bb78edfbe
Yannic Klem <yann.klem@gmail.com>
2016-02-11 17:25:40 +0100

    Delete items in lists with longtap or swipe to left

--- src/main/resources/public/app/item/item.js
+++ src/main/resources/public/app/item/item.js
@@ -1,6 +1,45 @@
-shoppingList.controller('itemController', ['$scope','listService','itemService',
-    function ($scope,listService,itemService) {
+shoppingList.controller('itemController', ['$scope','listService','itemService','$timeout','$rootScope',
+    function ($scope,listService,itemService, $timeout, $rootScope) {
         'use strict';
+
+        $scope.ctrl = $scope;
+        $scope.optionsVisible = false;
+        var optionsVisibleStopped = false;
+
+        $scope.showOptionsOnElement = function () {
+            if(!optionsVisibleStopped) {
+                $scope.optionsVisible = true;
+            }
+        };
+
+        $scope.hideOptionsOnElement = function () {
+            optionsVisibleStopped = true;
+            $scope.optionsVisible = false;
+
+            $timeout(function(){
+                optionsVisibleStopped = false;
+            },15);
+        };
+
+        $scope.showOptions = function () {
+            $scope.optionActive = true;
+
+            $timeout(function(){
+                $scope.optionsShown = true;
+                $scope.hideOptionsOnElement();
+            }, 20)
+        };
+
+        $scope.hideOptions = function () {
+            $scope.optionsShown = false;
+        };
+
+        $scope.$watch('optionsShown', function(newValue, oldValue){
+            if(oldValue && !newValue){
+                $scope.optionActive = false;
+                $scope.hideOptionsOnElement();
+            }
+        });
 
         $scope.deleteItem = function(item, event) {
 
