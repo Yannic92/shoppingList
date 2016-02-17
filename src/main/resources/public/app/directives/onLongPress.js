3eb007cc6f35af3e2d9e5dcc63dbcd7469a50222
Yannic Klem <yann.klem@gmail.com>
2016-02-17 20:52:07 +0100

    improved on-long-press directive

--- src/main/resources/public/app/directives/onLongPress.js
+++ src/main/resources/public/app/directives/onLongPress.js
@@ -3,27 +3,28 @@ shoppingList.directive('onLongPress', function($timeout) {
         restrict: 'A',
         link: function($scope, $elm, $attrs) {
             $elm.bind('touchstart', function(evt) {
-                // Locally scoped variable that will keep track of the long press
-                $scope.longPress = true;
                 $scope.longPressFinished = false;
 
-                // We'll set a timeout for 600 ms for a long press
-                $timeout(function() {
-                    if ($scope.longPress) {
-                        // If the touchend event hasn't fired,
-                        // apply the function given in on the element's on-long-press attribute
-                        $scope.$apply(function() {
-                            $scope.$eval($attrs.onLongPress)
-                        });
-                        $scope.longPressFinished = true;
-                    }
+                $scope.longPressTimeout = $timeout(function() {
+                    $scope.$apply(function() {
+                        $scope.$eval($attrs.onLongPress)
+                    });
+                    $scope.longPressFinished = true;
                 }, 600);
             });
 
+            $elm.bind('touchmove', function(evt){
+                if($scope.longPressTimeout) {
+                    $timeout.cancel($scope.longPressTimeout);
+                }
+            });
+
             $elm.bind('touchend', function(evt) {
-                // Prevent the onLongPress event from firing
-                $scope.longPress = false;
-                // If there is an on-touch-end function attached to this element, apply it
+
+                if($scope.longPressTimeout) {
+                    $timeout.cancel($scope.longPressTimeout);
+                }
+
                 if ($attrs.onTouchEnd) {
                     $scope.$apply(function() {
                         $scope.$eval($attrs.onTouchEnd)
